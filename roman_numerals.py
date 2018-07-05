cb009a47dbbfe6f56fa87e59d641e44be377ed36
Ben Carter <bencarter78@hotmail.com>
2018-07-05 23:51:24 +0100

    Making methods private

--- roman_numerals.py
+++ roman_numerals.py
@@ -28,42 +28,42 @@ class Numeral:
             return
 
         for index, i in enumerate(str(n)):
-            self.convert(int(i), self.MULTIPLYERS[len(str(n)) - index])
+            self._convert(int(i), self.MULTIPLYERS[len(str(n)) - index])
 
         return ''.join(self.numerals)
 
-    def convert(self, units, multiplyer):
+    def _convert(self, units, multiplyer):
         if units < 1:
             return
 
         if units in [4, 9]:
-            self.append(self.prefix(units, multiplyer))
-            return self.append((units + 1) * multiplyer)
+            self._append(self._prefix(units, multiplyer))
+            return self._append((units + 1) * multiplyer)
 
         if units % 5 == 0:
-            return self.append(units * multiplyer)
+            return self._append(units * multiplyer)
 
         if units > 5:
-            self.append(5 * multiplyer)
-            return self.convert(units % 5, multiplyer)
+            self._append(5 * multiplyer)
+            return self._convert(units % 5, multiplyer)
 
-        self.append(multiplyer)
-        self.convert(units - 1, multiplyer)
+        self._append(multiplyer)
+        self._convert(units - 1, multiplyer)
 
-    def append(self, index):
+    def _append(self, index):
         return self.numerals.append(self.LOOKUP[index])
 
-    def prefix(self, units, multiplyer):
+    def _prefix(self, units, multiplyer):
         if units * multiplyer <= 10:
             return 1
 
         keys = list(self.LOOKUP.keys())
         next_index = keys.index((units + 1) * multiplyer)
 
-        if self.is_multiple_of_ten((keys[next_index] / multiplyer)):
+        if self._is_multiple_of_ten((keys[next_index] / multiplyer)):
             return keys[next_index - 2]
 
         return keys[next_index - 1]
 
-    def is_multiple_of_ten(self, units):
+    def _is_multiple_of_ten(self, units):
         return units % 10 == 0
