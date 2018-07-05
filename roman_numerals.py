a705d956d86d1eaf80ffe9bd3c21516d4567cdfe
Ben Carter <bencarter78@hotmail.com>
2018-07-05 14:53:27 +0100

    Refactoring Pt 1

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,6 +1,9 @@
+from collections import OrderedDict
+
+
 class Numeral:
 
-    LOOKUP = {
+    LOOKUP = OrderedDict({
         1: 'I',
         5: 'V',
         10: 'X',
@@ -8,126 +11,63 @@ class Numeral:
         100: 'C',
         500: 'D',
         1000: 'M'
-    }
+    })
 
     def __init__(self):
         self.number_as_string = ''
         self.numeral = []
 
     def generate(self, n):
+        print("Converting %s" % n)
         """
         Generate the corresponding Roman Numeral for a given integer
         """
         self.number_as_string = str(n)
 
         if len(self.number_as_string) == 4:
-            self.thousands(int(self.number_as_string[0]))
+            self.calculate(int(self.number_as_string[0]), 1000)
             self.number_as_string = self.number_as_string[1:]
 
         if len(self.number_as_string) == 3:
-            self.hundreds(int(self.number_as_string[0]))
+            self.calculate(int(self.number_as_string[0]), 100)
             self.number_as_string = self.number_as_string[1:]
 
         if len(self.number_as_string) == 2:
-            self.tens(int(self.number_as_string[0]))
+            self.calculate(int(self.number_as_string[0]), 10)
             self.number_as_string = self.number_as_string[1:]
 
-        self.ones(int(self.number_as_string[0]))
+        self.calculate(int(self.number_as_string[0]), 1)
 
         return ''.join(self.numeral)
 
-    def ones(self, n):
+    def calculate(self, n, multiplyer):
         """
-        Determine the numerals for the ones column
+        Determine the numerals for the given column
         """
-        multiplyer = 1
-
         if n < 1:
             return
 
         if n in [4, 9]:
-            self.numeral.append(self.LOOKUP[1])
-            return self.numeral.extend(self.LOOKUP[n + multiplyer])
+            if n * multiplyer <= 10:
+                pre_numeral = self.LOOKUP[1]
+            else:
+                keys = list(self.LOOKUP.keys())
+                next_index = keys.index((n + 1) * multiplyer)
+                if (keys[next_index] / multiplyer) % 10 == 0:
+                    pre_numeral = self.LOOKUP[keys[next_index - 2]]
+                else:
+                    pre_numeral = self.LOOKUP[keys[next_index - 1]]
 
-        if n % 5 == 0:
-            return self.numeral.append(self.LOOKUP[n])
-
-        if n > 5:
-            self.numeral.append(self.LOOKUP[5])
-            return self.ones(n % 5)
-
-        self.numeral.append(self.LOOKUP[multiplyer])
-
-        self.ones(n - 1)
-
-    def tens(self, n):
-        """
-        Determine the numerals for the tens column
-        """
-        multiplyer = 10
-
-        if n < 1:
-            return
-
-        if n in [4, 9]:
-            self.numeral.append(self.LOOKUP[multiplyer])
+            self.numeral.append(pre_numeral)
             return self.numeral.extend(self.LOOKUP[(n + 1) * multiplyer])
 
         if n % 5 == 0:
             return self.numeral.append(self.LOOKUP[n * multiplyer])
 
         if n > 5:
-            self.numeral.append(self.LOOKUP[50])
-            return self.tens(n % 5)
+            self.numeral.append(self.LOOKUP[5 * multiplyer])
+            return self.calculate(n % 5, multiplyer)
 
         self.numeral.append(self.LOOKUP[multiplyer])
 
-        self.tens(n - 1)
-
-    def hundreds(self, n):
-        """
-        Determine the numerals for the hundreds column
-        """
-        multiplyer = 100
-
-        if n < 1:
-            return
-
-        if n in [4, 9]:
-            self.numeral.append(self.LOOKUP[multiplyer])
-            return self.numeral.extend(self.LOOKUP[(n + 1) * multiplyer])
-
-        if n % 5 == 0:
-            return self.numeral.append(self.LOOKUP[n * multiplyer])
-
-        if n > 5:
-            self.numeral.append(self.LOOKUP[500])
-            return self.hundreds(n % 5)
-
-        self.numeral.append(self.LOOKUP[multiplyer])
-
-        self.hundreds(n - 1)
-
-    def thousands(self, n):
-        """
-        Determine the numerals for the thousands column
-        """
-        multiplyer = 1000
-
-        if n < 1:
-            return
-
-        if n in [4, 9]:
-            self.numeral.append(self.LOOKUP[multiplyer])
-            return self.numeral.extend(self.LOOKUP[(n + 1) * multiplyer])
-
-        if n % 5 == 0:
-            return self.numeral.append(self.LOOKUP[n * multiplyer])
-
-        if n > 5:
-            self.numeral.append(self.LOOKUP[500])
-            return self.thousands(n % 5)
-
-        self.numeral.append(self.LOOKUP[multiplyer])
-
-        self.thousands(n - 1)
+        self.calculate(n - 1, multiplyer)
